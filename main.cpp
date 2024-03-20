2ddd71de6652c6f9eb2cec5635a5b0cd91335b29
luocan <2991826211@qq.com>
2024-03-20 23:04:04 +0800

    standard three basic, reference

--- main.cpp
+++ main.cpp
@@ -8,6 +8,33 @@
 #include <vector>
 using namespace std;
 
+class Base {
+public:
+    Base(int aa, int bb) : a (aa), b(bb) {
+        cout << "aa bb ctor base " << endl;
+    }
+
+    Base() {
+        cout << "base null ctor" << endl;
+    }
+public:
+    int a;
+    int b;
+};
+
+class Device : public Base {
+public:
+    Device() {
+        cout << "device ctor" << endl;
+    }
+
+    Device(int a, int b, int c, int d) : c(c), d(d), Base(a, b) {
+        cout << "device abdc ctor" << endl;
+    }
+public:
+    int c;
+    int d;
+};
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
@@ -20,7 +47,12 @@ int main() {
     Mystring::StringTest();
 
     TestConvertion();
-
+    Base a1;
+    cout << a1.a << a1.b << endl;
+    Base a2(1,2);
+    cout << a2.a << a2.b << endl;
+    Device d1;
+    Device d2(1,2,3,4);
     return 0;
 }
 
