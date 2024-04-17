3539618d515af6ac1b161b808abad8e47d43bf3f
luocan <2991826211@qq.com>
2024-04-17 21:55:27 +0800

    contains

--- main.cpp
+++ main.cpp
@@ -9,11 +9,19 @@
 #include "STL/common.h"
 #include "STL/arraytest.h"
 #include "STL/vectortest.h"
+#include <algorithm>
 #include <string>
 #include <sstream>
 #include <vector>
 #include <list>
+#include <deque>
+//#include <bits/stdc++.h>
+//#include <bits/stl_tree.h>
+#include <set>
+#include <map>
 
+
+using std::multiset;
 using namespace std;
 
 class A {
@@ -25,6 +33,23 @@ public:
 public:
     int a;
 };
+
+class MyComparator {
+public:
+    MyComparator() { cout << "ctor 11111" << endl;}
+    MyComparator(const MyComparator&) {
+        std::cout << "Copy Constructor\n";
+    }
+
+    MyComparator(MyComparator&&) noexcept {
+        std::cout << "Move Constructor\n";
+    }
+    ~MyComparator() {cout << "dtor 11111" << endl;}
+    bool operator()(int a, int b) {
+        return a > b;  // 降序比较
+    }
+};
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
 //    ComplexTesttMain();
@@ -44,13 +69,20 @@ int main() {
 //    TestNewDelete1();
 //    TestPlacementNewAndDelete();
 //    cout << sizeof(A) << " " << sizeof(A::B) << endl;
-    get_a_target_string();
-    arrayTest::test_array();
-    long value = 10000000;
-    long& vv = value;
-    vectortest::vector_test(value);
-    cout << sizeof(list<int>) << endl;
-    cout << sizeof(list<bool>) << endl;
+//    get_a_target_string();
+//    arrayTest::test_array();
+//    long value = 10000000;
+//    long& vv = value;
+//    vectortest::vector_test(value);
+//    cout << sizeof(list<int>) << endl;
+//    cout << sizeof(list<bool>) << endl;
+
+//    deque<int> dd {1,2,3,4,5};
+//    cout << dd[4] << endl;
+
+    vector<int> temp = {1,2,5,4,87,44};
+    sort(temp.begin(), temp.end(), MyComparator());
+    typedef MyComparator cc;
     return 0;
 }
 
